cc7eb4e36184497a3879aa2984c053a4add1c6cc
Mel <melissagarridosierra@gmail.com>
2026-04-16 07:32:54 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 document.getElementById("about").onclick = () => {
     window.location.href = "about.html";
 };
+
+const currentPage = window.location.pathname.split("/").pop() || "index.html";
+
+const links = document.querySelectorAll(".header a");
+const currentPage = window.location.pathname.split("/").pop();
+
+links.forEach(link => {
+    const linkPage = link.getAttribute("href");
+
+    if (linkPage === currentPage) {
+        link.classList.add("active");
+    }
+});
